9463460372a1c2c0bf1ffbcd238621b883b73d9f
jysperm <jysperm@gmail.com>
2014-02-17 20:17:23 +0800

    用户表的详细数据库结构

---
+++ DOC/DB/users.js
@@ -0,0 +1,26 @@
+user = {
+  _id: ObjectID('525284cc2cebb6d0008b4567'),
+  name: 'jysperm',
+  passwd: '53673f434686ce045477f066f30eded55a9bb535a6cec7b73a60972ccafddb2a',
+  passwd_salt: '53673f434686b535a6cec7b73a60ce045477f066f30eded55a9b972ccafddb2a',
+  email: 'jysperm@gmail.com',
+  signup_at: ISODate('2013-02-22T03:03:37.312Z'),
+  group: ['admin', 'stduser'],
+  setting: {
+    language: 'zh_CN',
+    QQ: '184300584'
+  },
+  attribute: {
+    expired: ISODate('2013-02-22T03:03:37.312Z')
+  },
+  tokens: [{
+      token: 'b535a6cec7b73a60c53673f434686e04972ccafddb2a5477f066f30eded55a9b',
+      available: true,
+      created_at: ISODate('2013-02-22T03:03:37.312Z'),
+      updated_at: ISODate('2013-02-22T03:03:37.312Z'),
+      attribute: {
+        ip: '123.184.237.163',
+        ua: 'Mozilla/5.0 (Intel Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102'
+      }
+  }]
+};
