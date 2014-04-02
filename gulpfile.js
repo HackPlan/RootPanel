880acf3baefdff8875715b877c4284410e10bf46
Yudong <liyudong1992@gmail.com>
2014-04-02 19:36:33 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,16 @@ var path = require('path');
 
 lessPath = './core/static/style';
 coffeePath = './core/static/script';
+corePath = './core'
 
 gulp.task('less', function() {
   return gulp.src(lessPath + "/**/*.less").pipe(less()).pipe(gulp.dest(lessPath));
 });
 
 gulp.task('coffee', function() {
+  gulp.src(corePath + "/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
+    throw error;
+  }).pipe(uglify()).pipe(gulp.dest(corePath));
   return gulp.src(coffeePath + "/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
     throw error;
   }).pipe(uglify()).pipe(gulp.dest(coffeePath));
@@ -19,6 +23,7 @@ gulp.task('coffee', function() {
 
 gulp.task('watch', function(){
   gulp.watch(coffeePath + "/**/*.coffee",['coffee']);
+  gulp.watch(corePath + "/**/*.coffee",['coffee']);
   gulp.watch(lessPath + "/**/*.less",['less']);
 });
 
