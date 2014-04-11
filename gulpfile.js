839a8099500f4feba74b34cccf53d260c7d6fd72
Yudong <liyudong1992@gmail.com>
2014-04-11 18:42:55 +0800

    fix #afefe03

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var uglify = require('gulp-uglify');
 var path = require('path');
 
 lessPath = './core/static/style';
-coffeePath = './core/static/script';
 corePath = './core'
 
 gulp.task('less', function() {
@@ -13,17 +12,13 @@ gulp.task('less', function() {
 });
 
 gulp.task('coffee', function() {
-  gulp.src(corePath + "/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
+  return gulp.src(corePath + "/**/**/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
     throw error;
   }).pipe(uglify()).pipe(gulp.dest(corePath));
-  return gulp.src(coffeePath + "/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
-    throw error;
-  }).pipe(uglify()).pipe(gulp.dest(coffeePath));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(coffeePath + "/**/*.coffee",['coffee']);
-  gulp.watch(corePath + "/**/*.coffee",['coffee']);
+  gulp.watch(corePath + "/**/**/**/*.coffee",['coffee']);
   gulp.watch(lessPath + "/**/*.less",['less']);
 });
 
