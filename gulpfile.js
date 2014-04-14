4a008f910c1d87feadbe615f56747f85c3902fce
Yudong <liyudong1992@gmail.com>
2014-04-15 00:49:22 +0800

    彻底解决gulpfile目录问题

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ var uglify = require('gulp-uglify');
 var path = require('path');
 
 lessPath = './core/static/style';
-corePath = './core'
+corePath = './'
 
 gulp.task('less', function() {
   return gulp.src(lessPath + "/**/*.less").pipe(less()).pipe(gulp.dest(lessPath));
 });
 
 gulp.task('coffee', function() {
-  return gulp.src(corePath + "/**/**/**/*.coffee").pipe(coffee({bare:true})).on('error', function(error) {
+  return gulp.src([corePath + "/**/*.coffee",'!node_modules/**/*.coffee']).pipe(coffee({bare:true})).on('error', function(error) {
     throw error;
   }).pipe(uglify()).pipe(gulp.dest(corePath));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(corePath + "/**/**/**/*.coffee",['coffee']);
+  gulp.watch([corePath + "/**/*.coffee",'!node_modules/**/*.coffee'],['coffee']);
   gulp.watch(lessPath + "/**/*.less",['less']);
 });
 
